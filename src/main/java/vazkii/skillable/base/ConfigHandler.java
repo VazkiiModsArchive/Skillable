f2330a73a883d2a044a30652167e950f15b4e5f9
Vasco Lavos <vazkii@hotmail.com>
2017-04-27 18:32:32 +0100

    Description for the skill lock format

--- src/main/java/vazkii/skillable/base/ConfigHandler.java
+++ src/main/java/vazkii/skillable/base/ConfigHandler.java
@@ -30,15 +30,25 @@ public class ConfigHandler {
 	}
 
 	public static void load() {
-		String desc;
-
 		baseXPCost = loadPropInt("Base XP Cost", "", baseXPCost);
 		xpIncrease = loadPropInt("XP Increase Per Level", "", 1);
 		skillPointInterval = loadPropInt("Levels per Skill Point", "", skillPointInterval);
 		levelCap = loadPropInt("Level Cap", "", levelCap);
 		disableSheepWool = loadPropBool("Disable Sheep Dropping Wool on Death", "", disableSheepWool);
 
-		String[] locks = config.getStringList("Skill Locks", Configuration.CATEGORY_GENERAL, LevelLockHandler.DEFAULT_SKILL_LOCKS, "");
+		String desc = "Set requirements for items in this list. Each entry is composed of the item key and the requirements\n"
+				+ "The item key is in the simple mod:item_id format. Optionally, it can be in mod:item_id:metadata, if you want to match metadata.\n"
+				+ "The requirements are in a comma separated list, each in a key:value format. For example, to make an iron pickaxe require 5 mining\n"
+				+ "and 5 building, you'd use the following string:\n"
+				+ "\"minecraft:iron_pickaxe=mining:5,building:5\"\n\n"
+				+ "Item usage can also be locked behind an achievement, by using ach:id. For example, to make the elytra require the \"The End.\" achievement\n"
+				+ "you'd use the following string:\n"
+				+ "\"minecraft:elytra=ach:theEnd2\"\n\n"
+				+ "Skill requirements and achievements can be mixed and matched, so you can make an item require both, if you want.\n"
+				+ "You can also lock placed blocks from being used or broken, in the same manner.\n\n"
+				+ "Locks defined here apply to all the following cases: Right clicking an item, placing a block, breaking a block, using a block that's placed,\n"
+				+ "left clicking an item, using an item to break any block, and equipping an armor item.\n";
+		String[] locks = config.getStringList("Skill Locks", Configuration.CATEGORY_GENERAL, LevelLockHandler.DEFAULT_SKILL_LOCKS, desc);
 		LevelLockHandler.loadFromConfig(locks);
 		
 		Skills.ALL_UNLOCKABLES.clear();
