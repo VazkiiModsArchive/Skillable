f1a6cc00bdbbb41f1b6fa44e06f23890034bf7c9
Vasco Lavos <vazkii@hotmail.com>
2017-05-27 22:40:10 +0100

    Fixed a dupe with perfect recover and silk touch.

--- src/main/java/vazkii/skillable/skill/building/TraitPerfectRecover.java
+++ src/main/java/vazkii/skillable/skill/building/TraitPerfectRecover.java
@@ -1,6 +1,8 @@
 package vazkii.skillable.skill.building;
 
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Enchantments;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.world.BlockEvent.HarvestDropsEvent;
@@ -15,17 +17,19 @@ public class TraitPerfectRecover extends Trait {
 
 	@Override
 	public void onBlockDrops(HarvestDropsEvent event) { 
-		if(event.getState().getBlock() == Blocks.GLOWSTONE) {
-			event.getDrops().removeIf((s) -> s.getItem() == Items.GLOWSTONE_DUST);
-			event.getDrops().add(new ItemStack(Items.GLOWSTONE_DUST, 4));
+		if(EnchantmentHelper.getEnchantmentLevel(Enchantments.SILK_TOUCH, event.getHarvester().getActiveItemStack()) == 0) {
+			if(event.getState().getBlock() == Blocks.GLOWSTONE) {
+				event.getDrops().removeIf((s) -> s.getItem() == Items.GLOWSTONE_DUST);
+				event.getDrops().add(new ItemStack(Items.GLOWSTONE_DUST, 4));
+			}
+			else if(event.getState().getBlock() == Blocks.SEA_LANTERN) {
+				event.getDrops().removeIf((s) -> s.getItem() == Items.PRISMARINE_CRYSTALS);
+				event.getDrops().add(new ItemStack(Items.PRISMARINE_CRYSTALS, 5));
+				event.getDrops().add(new ItemStack(Items.PRISMARINE_SHARD, 4));
+			} else for(ItemStack stack : event.getDrops())
+				if(stack.getItem().getRegistryName().toString().equals("quark:glass_shards") && stack.getCount() < 4)
+					stack.setCount(4);
 		}
-		else if(event.getState().getBlock() == Blocks.SEA_LANTERN) {
-			event.getDrops().removeIf((s) -> s.getItem() == Items.PRISMARINE_CRYSTALS);
-			event.getDrops().add(new ItemStack(Items.PRISMARINE_CRYSTALS, 5));
-			event.getDrops().add(new ItemStack(Items.PRISMARINE_SHARD, 4));
-		} else for(ItemStack stack : event.getDrops())
-			if(stack.getItem().getRegistryName().toString().equals("quark:glass_shards") && stack.getCount() < 4)
-				stack.setCount(4);
 	}
 
 }
