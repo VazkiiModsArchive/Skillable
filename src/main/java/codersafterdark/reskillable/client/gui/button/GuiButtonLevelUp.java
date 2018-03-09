b1f48c7b359b5b7a753331577ad5d4d4dc529ed0
Jared <jaredlll08@gmail.com>
2018-03-10 01:31:35 +0200

    Made the button fade in and out Close #11

--- src/main/java/codersafterdark/reskillable/client/gui/button/GuiButtonLevelUp.java
+++ src/main/java/codersafterdark/reskillable/client/gui/button/GuiButtonLevelUp.java
@@ -6,37 +6,44 @@ import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.renderer.GlStateManager;
 
 public class GuiButtonLevelUp extends GuiButton {
-
+    
     int cost;
-
+    
     public GuiButtonLevelUp(int x, int y) {
         super(0, x, y, 14, 14, "");
         cost = Integer.MAX_VALUE;
     }
-
+    
     public void setCost(int cost) {
         this.cost = cost;
     }
-
+    
+    
+    float renderTicks = 0;
+    
     @Override
     public void drawButton(Minecraft mc, int mouseX, int mouseY, float f) {
         enabled = mc.player.experienceLevel >= cost || mc.player.isCreative();
-        if (enabled) {
+        if(enabled) {
             GlStateManager.color(1F, 1F, 1F);
             mc.renderEngine.bindTexture(GuiSkillInfo.SKILL_INFO_RES);
-
+            
             int x = this.x;
             int y = this.y;
             int u = 176;
             int v = 0;
             int w = width;
             int h = height;
-
-            if (mouseX > this.x && mouseY > this.y && mouseX < this.x + width && mouseY < this.y + height)
+            
+            if(mouseX > this.x && mouseY > this.y && mouseX < this.x + width && mouseY < this.y + height) {
                 v += h;
-
+            } else {
+                float speedModifier = 4;
+                GlStateManager.color(1, 1, 1, (float) (Math.sin(mc.player.ticksExisted / speedModifier) + 1) / 2);
+                
+            }
             drawTexturedModalRect(x, y, u, v, w, h);
         }
     }
-
+    
 }
