9374c6819902292aeb850ab21d8ae8e65aaa091c
Lanse <simon_stalnabb@hotmail.com>
2018-03-10 15:57:18 +0100

    Added JEI, Waila, CRT to the Dev Enviroment. Also started work on CrT Requirement Support

--- src/main/java/codersafterdark/reskillable/compat/crafttweaker/RequirementTweaker.java
+++ src/main/java/codersafterdark/reskillable/compat/crafttweaker/RequirementTweaker.java
@@ -1,4 +1,45 @@
 package codersafterdark.reskillable.compat.crafttweaker;
 
+import codersafterdark.reskillable.base.RequirementHolder;
+import crafttweaker.CraftTweakerAPI;
+import crafttweaker.annotations.ZenRegister;
+import crafttweaker.api.item.IItemStack;
+import crafttweaker.api.minecraft.CraftTweakerMC;
+import net.minecraft.item.ItemStack;
+import stanhebben.zenscript.annotations.ZenClass;
+import stanhebben.zenscript.annotations.ZenMethod;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ZenClass("mods.reskillable.Requirement")
+@ZenRegister
 public class RequirementTweaker {
+    private static final Map<ItemStack, RequirementHolder> locks = new HashMap();
+
+    @ZenMethod
+    public static void addRequirement(IItemStack item, String locked){
+        if (locked == null || locked.isEmpty()){
+            CraftTweakerAPI.logError("String: " + locks + " was found to be either null or empty!");
+        } else if (item == null || item.isEmpty()){
+            CraftTweakerAPI.logError("Itemstack: " + item + " was found to be either null or empty!");
+        } else {
+            ItemStack realItem = CraftTweakerMC.getItemStack(item);
+            locks.clear();
+            lock = locked;
+
+            if (locked == null){
+                return;
+            }
+
+            for (String s : locked) {
+                String[] tokens = s.split("=");
+                if (tokens.length == 2) {
+                    RequirementHolder h = RequirementHolder.fromString(tokens[1]);
+                    locks.put(realItem, h);
+                }
+            }
+        }
+    }
 }
+
