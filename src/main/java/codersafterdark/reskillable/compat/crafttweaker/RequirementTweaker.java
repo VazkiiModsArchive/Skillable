f524880726430b884a13a64c25b8a866969b856c
Jared <jaredlll08@gmail.com>
2018-03-11 19:08:16 +0200

    Fixed Crafttweaker support and added nbt locking. Close #12 and #21

--- src/main/java/codersafterdark/reskillable/compat/crafttweaker/RequirementTweaker.java
+++ src/main/java/codersafterdark/reskillable/compat/crafttweaker/RequirementTweaker.java
@@ -1,45 +1,42 @@
 package codersafterdark.reskillable.compat.crafttweaker;
 
-import codersafterdark.reskillable.base.RequirementHolder;
-import crafttweaker.CraftTweakerAPI;
+import codersafterdark.reskillable.base.*;
+import crafttweaker.*;
 import crafttweaker.annotations.ZenRegister;
 import crafttweaker.api.item.IItemStack;
 import crafttweaker.api.minecraft.CraftTweakerMC;
 import net.minecraft.item.ItemStack;
-import stanhebben.zenscript.annotations.ZenClass;
-import stanhebben.zenscript.annotations.ZenMethod;
-
-import java.util.HashMap;
-import java.util.Map;
+import stanhebben.zenscript.annotations.*;
 
 @ZenClass("mods.reskillable.Requirement")
 @ZenRegister
 public class RequirementTweaker {
-    private static final Map<ItemStack, RequirementHolder> locks = new HashMap();
-
+    
     @ZenMethod
-    public static void addRequirement(IItemStack item, String locked){
-        if (locked == null || locked.isEmpty()){
-            CraftTweakerAPI.logError("String: " + locks + " was found to be either null or empty!");
-        } else if (item == null || item.isEmpty()){
-            CraftTweakerAPI.logError("Itemstack: " + item + " was found to be either null or empty!");
-        } else {
-            ItemStack realItem = CraftTweakerMC.getItemStack(item);
-            locks.clear();
-            lock = locked;
-
-            if (locked == null){
-                return;
-            }
-
-            for (String s : locked) {
-                String[] tokens = s.split("=");
-                if (tokens.length == 2) {
-                    RequirementHolder h = RequirementHolder.fromString(tokens[1]);
-                    locks.put(realItem, h);
+    public static void addRequirement(IItemStack item, String locked) {
+        CraftTweakerAPI.apply(new IAction() {
+            @Override
+            public void apply() {
+                if(locked == null || locked.isEmpty()) {
+                    CraftTweakerAPI.logError("String: " + locked + " was found to be either null or empty!");
+                    return;
                 }
+                if(item == null || item.isEmpty()) {
+                    CraftTweakerAPI.logError("Itemstack: " + item + " was found to be either null or empty!");
+                    return;
+                }
+                ItemStack realItem = CraftTweakerMC.getItemStack(item);
+    
+                RequirementHolder h = RequirementHolder.fromString(locked);
+                LevelLockHandler.craftTweakerLocks.put(realItem, h);
             }
-        }
+    
+            @Override
+            public String describe() {
+                return "Setting the requirement of: " + item + " to: " + locked;
+            }
+        });
+        
     }
 }
 
