eb129df80845324a4e70ba41a32bbe02e9b24bf7
Jared <jaredlll08@gmail.com>
2018-03-11 20:09:54 +0200

    added license to RenderHelper

--- src/main/java/codersafterdark/reskillable/client/base/RenderHelper.java
+++ src/main/java/codersafterdark/reskillable/client/base/RenderHelper.java
@@ -1,3 +1,13 @@
+/**
+ * This class was created by <Vazkii>. It's distributed as
+ * part of the Botania Mod. Get the Source Code in github:
+ * https://github.com/Vazkii/Botania
+ *
+ * Botania is Open Source and distributed under the
+ * Botania License: http://botaniamod.net/license.php
+ *
+ * File Created @ [Jan 19, 2014, 5:40:38 PM (GMT)]
+ */
 package codersafterdark.reskillable.client.base;
 
 import net.minecraft.client.Minecraft;
