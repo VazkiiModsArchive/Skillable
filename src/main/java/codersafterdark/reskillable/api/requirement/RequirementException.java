07ae04aceea1867f4c9b093b912fdcfd4b8a34f3
pupnewfster <richard@freimer.com>
2018-06-11 17:02:38 -0400

    Make it possible so that Requirements can set syntax error messages

---
+++ src/main/java/codersafterdark/reskillable/api/requirement/RequirementException.java
@@ -0,0 +1,7 @@
+package codersafterdark.reskillable.api.requirement;
+
+public class RequirementException extends Exception {
+    public RequirementException(String message) {
+        super(message);
+    }
+}
