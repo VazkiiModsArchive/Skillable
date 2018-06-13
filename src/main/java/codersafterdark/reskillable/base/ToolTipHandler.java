927c024136957b88079bdf8598eaf9c3dd684fe5
pupnewfster <richard@freimer.com>
2018-06-13 11:53:41 -0400

    Create tooltip injection to allow for addons to include extra information in tooltips

--- src/main/java/codersafterdark/reskillable/base/ToolTipHandler.java
+++ src/main/java/codersafterdark/reskillable/base/ToolTipHandler.java
@@ -8,6 +8,7 @@ import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraftforge.client.event.GuiOpenEvent;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.network.FMLNetworkEvent;
@@ -15,10 +16,15 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 
 public class ToolTipHandler {
+    private static Map<Class<? extends GuiScreen>, Function<Boolean, List<String>>> tooltipInjectors = new HashMap<>();
     private static RequirementHolder lastLock = LevelLockHandler.EMPTY_LOCK;
+    private static Class<? extends GuiScreen> currentGui;
     private static List<String> toolTip = new ArrayList<>();
     private static ItemStack lastItem;
     private static boolean enabled;
@@ -42,14 +48,32 @@ public class ToolTipHandler {
             lastLock = LevelLockHandler.getSkillLock(current);
             lastLock.addRequirementsIgnoreShift(data, toolTip = new ArrayList<>());
         }
+        boolean showDetails = !ConfigHandler.hideRequirements || GuiScreen.isShiftKeyDown();
         List<String> curTooltip = event.getToolTip();
-        if (!toolTip.isEmpty()) {
-            if (!ConfigHandler.hideRequirements || GuiScreen.isShiftKeyDown()) {
+        List<String> extraToolTips = new ArrayList<>();
+
+        if (currentGui != null) {
+            //TODO: If an addon/set of addons ever want to both inject tooltips for the same class make tooltipInjectors hold a list of functions
+            extraToolTips = tooltipInjectors.get(currentGui).apply(showDetails);
+            //TODO: Try to cache the return somehow. If so the function would need some way to return two lists one with showDetails one without
+            //TODO Cont: Another way would be reset it when item changes and gui changes AND also invalidate it if shift state changes and config option not set
+        }
+        if (!toolTip.isEmpty() || !extraToolTips.isEmpty()) {
+            if (showDetails) {
                 curTooltip.add(TextFormatting.DARK_PURPLE + new TextComponentTranslation("skillable.misc.skillLock").getUnformattedComponentText());
                 curTooltip.addAll(toolTip);
             } else {
                 curTooltip.add(TextFormatting.DARK_PURPLE + new TextComponentTranslation("skillable.misc.skillLockShift").getUnformattedComponentText());
             }
+            curTooltip.addAll(extraToolTips);
+        }
+    }
+
+    @SubscribeEvent
+    @SideOnly(Side.CLIENT)
+    public void onGuiOpen(GuiOpenEvent event) {
+        if (enabled && !event.isCanceled()) {
+            currentGui = event.getGui() == null ? null : event.getGui().getClass();
         }
     }
 
