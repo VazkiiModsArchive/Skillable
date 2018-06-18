86e8f2e9180ba6a0844466cab8c72cbdf1361afd
pupnewfster <richard@freimer.com>
2018-06-18 15:40:57 -0400

    Work on allowing custom sprites to be set instead of having to have them all in one file

--- src/main/java/codersafterdark/reskillable/client/gui/GuiSkills.java
+++ src/main/java/codersafterdark/reskillable/client/gui/GuiSkills.java
@@ -45,9 +45,17 @@ public class GuiSkills extends GuiScreen {
 
     public static void drawSkill(int x, int y, Skill skill) {
         Minecraft mc = Minecraft.getMinecraft();
-        mc.renderEngine.bindTexture(skill.getSpriteLocation());
-        Pair<Integer, Integer> pair = skill.getSpriteFromRank(PlayerDataHandler.get(mc.player).getSkillInfo(skill).getRank());
-        RenderHelper.drawTexturedModalRect(x, y, 1, pair.getKey(), pair.getValue(), 16, 16, 1f / 64, 1f / 64);
+        int rank = PlayerDataHandler.get(mc.player).getSkillInfo(skill).getRank();
+        if (skill.hasCustomSprites()) {
+            ResourceLocation sprite = skill.getSpriteLocation(rank);
+            if (sprite != null) {
+                mc.renderEngine.bindTexture(sprite);
+            }
+        } else {
+            mc.renderEngine.bindTexture(skill.getSpriteLocation());
+            Pair<Integer, Integer> pair = skill.getSpriteFromRank(rank);
+            RenderHelper.drawTexturedModalRect(x, y, 1, pair.getKey(), pair.getValue(), 16, 16, 1f / 64, 1f / 64);
+        }
     }
 
     public static void drawScrollButtonsTop(int x, int y) {
